39dafb35540e1b7a51568ad71a1f39c93eb747e3
Candice Bianca Brown <candicebrown97@gmail.com>
2017-08-01 23:18:03 +0000

    completed API HW #2

--- youtubeAPI#1.js
+++ youtubeAPI#1.js
@@ -1,10 +1,39 @@
-// Add your code below this line!
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://www.codecademy.com/", false);
-xhr.send();
+// Your use of the YouTube API must comply with the Terms of Service:
+// https://developers.google.com/youtube/terms
 
+// Helper function to display JavaScript value on HTML page.
+function showResponse(response) {
+    var responseString = JSON.stringify(response, '', 2);
+    document.getElementById('response').innerHTML += responseString;
+}
 
-// Add your code above this line!
+// Called automatically when JavaScript client library is loaded.
+function onClientLoad() {
+    gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
+}
 
-console.log(xhr.status);
-console.log(xhr.statusText);
+// Called automatically when YouTube API interface is loaded (see line 9).
+function onYouTubeApiLoad() {
+    // This API key is intended for use only in this lesson.
+    // See https://goo.gl/PdPA1 to get a key for your own applications.
+    gapi.client.setApiKey('AIzaSyCR5In4DZaTP6IEZQ0r1JceuvluJRzQNLE');
+
+    search();
+}
+
+function search() {
+    // Use the JavaScript client library to create a search.list() API call.
+    var request = gapi.client.youtube.search.list({
+        part: 'snippet',
+        q: 'API Tutorials'
+    });
+    
+    // Send the request to the API server,
+    // and invoke onSearchRepsonse() with the response.
+    request.execute(onSearchResponse);
+}
+
+// Called automatically with the response of the YouTube API request.
+function onSearchResponse(response) {
+    showResponse(response);
+}
